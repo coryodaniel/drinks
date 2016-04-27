827df9896861767cbaa3beda66f3f21d79b8f859
Cory O'Daniel <cory@coryodaniel.com>
2016-04-27 11:30:12 -0700

    * added ingredients route * added ingredient resource * added name validation * stubbed out application controller for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
-class ApplicationController < ActionController::API
+class ApplicationController < JSONAPI::ResourceController
+  def context
+    {current_user: current_user}
+  end
+
+  def current_user
+    nil
+  end
 end
